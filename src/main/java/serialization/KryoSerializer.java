888751801b63e87e0e756f5a9c512cbc32ec166f
AIminminAI <72067353+AIminminAI@users.noreply.github.com>
2022-11-19 10:48:06 +0800

    Update KryoSerializer.java

--- src/main/java/serialization/KryoSerializer.java
+++ src/main/java/serialization/KryoSerializer.java
@@ -16,8 +16,11 @@ public class KryoSerializer implements Serializer{
     /**
      * 由于Kryo不是线程安全的，所以每个线程都应该有自己的Kryo，Input和Output实例
      */
+    //Java8中ThreadLocal对象提供了一个Lambda构造方式，实现了非常简洁的构造方法：withInitial。这个方法采用Lambda方式传入实现了 Supplier 函数接口的参数。用ThreadLocal作为容器，当每个线程访问这个 kryoThreadLocal 变量时，ThreadLocal会为每个线程提供一份kryoThreadLocal变量，就可以保证各个线程互不影响
+    //其实ThreadLocal.withInitial(()...)感觉就跟ThreadLocal t = new ThreadLocal要表达的意思一样
     private static final ThreadLocal<Kryo> kryoThreadLocal = ThreadLocal.withInitial(()->{
         Kryo kryo = new Kryo();
+        //If we need more control over the serialization process, we have two options; we can write our own Serializer class and register it with Kryo or let the class handle the serialization by itself.
         kryo.register(RpcResponse.class);
         kryo.register(RpcRequest.class);
         kryo.setReferences(true);//默认值为true，表示是否关闭注册行为，关闭之后可能存在序列化问题，一般推荐设置为true
